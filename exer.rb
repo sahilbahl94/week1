032784b55738bbf24444d430aae9126e1c3c5216
Sahil Bahl <sahilbahl94@gmail.com>
2015-10-12 16:27:27 +0200

    Iteration One done

---
+++ exer.rb
@@ -0,0 +1,45 @@
+class Car 
+
+	def initialize(sound)
+		@sound = sound
+	end
+
+	def make_noise
+		puts @sound
+	end
+end
+
+
+
+=begin
+	
+cars = sounds.map do |sound|
+	Car.new(sound)
+end
+
+cars.each do |car|
+	car.make_noise
+end
+
+=end
+
+#Exercise 3
+
+sounds = ["Broom", "Meek", "Nyan"]
+
+cars = sounds.each_with_object({}) do |sound,memo|
+	memo[sound.to_sym] = Car.new(sound)
+end
+
+#EX 4
+newcars = [Car.new("Broom"), Car.new("Meek"), Car.new("Nyan")]
+
+string = newcars.reduce("") do |memo, car|
+	memo += car.sound
+end
+
+
+
+
+
+
---
+++ pair_programming.rb
@@ -0,0 +1,41 @@
+class FlashCard
+  attr_accessor :question, :answer
+
+  def initialize(question, answer)
+  @question = question
+    @answer = answer
+  end
+
+  def question_and_answer(index = nil)
+      if @answer == ""
+          return
+      end         
+      puts " #{index} #{@question}"
+      input = ''
+      while input != @answer
+          input = gets.chomp
+      end
+      puts @answer
+  end
+end
+
+def display_cards(cards)
+    cards.each_with_index do |card, index|
+        card.question_and_answer(index)
+    end
+end
+
+card0 = FlashCard.new("What does the array.each method do", "Iterates through each element in the array and performs an operation on them")
+card01 = FlashCard.new("How much wood could a wood chuck chuck if a wood chuck could chuck wood", "42")
+card1 = FlashCard.new("What does the array.each_with_index method do", "Iterates through each element in the array and keeps track of index of the current element")
+card2 = FlashCard.new("What does the array.map method do", "Returns an array with operation performed on the original array")
+card5 = FlashCard.new("What does the array.select method do", "Returns an array that is a subset of the original array based on the computation in the block.")
+card02 = FlashCard.new("What is my favorite food", "Whiskey")
+card3 = FlashCard.new("What does the array.reduce method do", "Shortens the array into a single copmuted value")
+card03 = FlashCard.new("Why is the sky blue", "Because, science.")
+
+card_list = [card0, card01, card1, card02, card2, card3, card03, card5]
+
+card0.question_and_answer(0)
+
+display_cards(card_list)
